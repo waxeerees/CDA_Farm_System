da035e84f28d6f917713fdad6c8c5498396235c9
waxeerees <git@github.com:waxeeres>
2023-05-18 07:23:17 +0100

    First push

---
+++ CDA_Farm_System/urls.py
@@ -0,0 +1,34 @@
+"""CDA_Farm_System URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import path, include, re_path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('customer.urls')),
+    path('api/', include('reportingApp.urls')),
+    path('api/', include('customer.urls')),
+    path('api/', include('commerce.urls')),
+    path('api/', include('management.urls')),
+    path('api/', include('userApp.urls')),
+    path('api/', include('django.contrib.auth.urls')),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
+   
+]
+
